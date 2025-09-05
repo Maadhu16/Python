bc70938e3d9605ae16ed079e77cb70bbab869abd
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-05 16:43:25 +0530

    Create If_&_else.py

---
+++ Conditional_statement/If_&_else.py
@@ -0,0 +1,21 @@
+# IF - execute a block of code only if a condition is True.
+
+# Example: 
+
+a = 10
+if a == 10:
+  print("True")
+
+Output => True
+
+# Else - runs when the if condition is False.
+
+# Example: 
+
+a = 10
+if a == 5:
+  print("True")
+else:
+  print("False")
+
+Output => False
