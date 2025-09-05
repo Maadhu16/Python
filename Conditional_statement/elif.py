86988175f3552987c72b8d881970a90f44aa6463
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-05 16:50:23 +0530

    Create elif.py

---
+++ Conditional_statement/elif.py
@@ -0,0 +1,15 @@
+# Elif - (else if) Used when you want to check multiple conditions.
+
+# Example:
+
+a = 75 
+if a >= 90:
+  print("Grade A")
+elif a >= 75:
+  print("Grade B")
+elif a >= 35:
+  print("Grade C")
+else:
+  print("Fail")
+
+Output => Grade B
